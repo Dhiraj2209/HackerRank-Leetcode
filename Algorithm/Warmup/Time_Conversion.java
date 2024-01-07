a14ed14ba802c0fa5bcdb25ad3251e14cb3ce10a
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-01-07 22:43:59 +0530

    Updated Time_Conversion.java

--- Algorithm/Warmup/Time_Conversion.java
+++ Algorithm/Warmup/Time_Conversion.java
@@ -1,19 +1,6 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 class Result {
-
-    /*
-     * Complete the 'timeConversion' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts STRING s as parameter.
-     */
 
     public static String timeConversion(String s) {
     // Write your code here
@@ -40,7 +27,7 @@ class Result {
 
 }
 
-public class Solution {
+public class Time_Conversion {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
