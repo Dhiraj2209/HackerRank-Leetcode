9e40da6e37c268a50490c5a7a760a7a28cb2a100
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-05-31 21:32:21 +0530

    Big Integer class in java

---
+++ JAVA/JavaBigInteger.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        String num1 = sc.nextLine();
+        String num2 = sc.nextLine();
+        
+        BigInteger bnt1 = new BigInteger(num1);
+        BigInteger bnt2 = new BigInteger(num2);
+        
+        BigInteger sum = bnt1.add(bnt2);
+        System.out.println(sum);
+        
+        System.out.println(bnt1.multiply(bnt2));
+    }
+}
