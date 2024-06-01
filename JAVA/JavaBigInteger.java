dbd3756cf4fe2d8091cdb0db75f4d5811eb37898
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-06-01 23:42:19 +0530

    PS added

--- JAVA/JavaBigInteger.java
+++ JAVA/JavaBigInteger.java
@@ -5,7 +5,9 @@ import java.math.BigInteger;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution.
+
+input two big integer and print sum and multiplication of that big Integers.  */
         Scanner sc = new Scanner(System.in);
         
         String num1 = sc.nextLine();
